cbeee70db9460c16222258bdaf3eb5b1c5de9614
Alex Oliveira <alexoliveirasince1989@hotmail.com>
2020-03-31 23:14:32 -0300

    modificações nas mensagens

--- arquivo.sh
+++ arquivo.sh
@@ -14,7 +14,7 @@ echo "Meu nome é $nome e eu tenho $idade anos de idade."
 
 if [[ $idade -lt 70 ]]
 then
-  echo "Parabéns!! Você não está no grupo de risco."
+  echo "Você não está no grupo de risco."
 else
-  echo "Pode encomendar o caixão."
+  echo "Cuidado! Você está no grupo de risco."
 fi
